147d63ad2a7f23e8a68a1293cee0e6f55e754ac6
Doug Gosciak <gosciak@mac.com>
2014-10-15 15:29:52 -0400

    Initial commit

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,18 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Doug Gosciak on 2014/10/14.
+//  Copyright (c) 2014 Doug Gosciak. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named: "")
+    
+}
